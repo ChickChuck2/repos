db1c8ec2e64385b849f49a57d6cd5b98d157393a
ChickChuck2 <48648882+ChickChuck2@users.noreply.github.com>
2021-03-23 11:56:08 -0300

    sexo

--- new.py
+++ new.py
@@ -1,1 +1,61 @@
-print("sexo")
+import sys
+from typing import Text
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPixmap
+from PyQt5 import QtGui
+import random
+import time
+
+# PyQt5 tem sua propia função de data e horas
+
+#>Variaveis<#
+default = 'style.css'
+
+width = 800
+height = 600
+
+class Janela(QMainWindow):
+    def __init__(self):
+        super(Janela, self).__init__()
+
+        #<!>Main<!>#
+        #Titulo da janela
+        self.setWindowTitle("Example")
+        #Coloca um icone na janela
+        self.setWindowIcon(QtGui.QIcon('icon.ico'))
+        #tamanho da janela
+        self.setGeometry(0,0,width,height)
+
+        # <--- Text --->
+
+        # >>Style<<
+        
+        #!!Important!!
+        self.show()
+    
+    #!@ Funções @!
+
+    #player papel
+
+# cor da janela em (CSS)
+df = """
+    Janela {
+        background-color: green;
+    }
+"""
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    # aqui você coloca o estilo da janela
+    app.setStyleSheet(df)
+
+    #define a janela
+    janela = Janela()
+
+    #mostra a janela
+    janela.show()
+
+    #Função para quando apertar no X pra sair
+    sys.exit(app.exec_())
